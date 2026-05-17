408be6ef148f92e218d89009c2dabf28b4e2adc6
Czarina Asuncion <czarinaasuncion7197@gmail.com>
2026-05-17 16:59:09 +0800

    Add files via upload

---
+++ src/NobleGas.java
@@ -0,0 +1,6 @@
+//childclass
+class NobleGas extends Gas {
+    public NobleGas(String name, String symbol, int atomicNumber, double atomicMass, String electronConfig, String realWorldUse) {
+        super(name, symbol, atomicNumber, atomicMass, electronConfig, realWorldUse);
+    }
+}
