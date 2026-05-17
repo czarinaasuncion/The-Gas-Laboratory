6c9d0d437258289e23d9638188b9136da03f1505
Czarina Asuncion <czarinaasuncion7197@gmail.com>
2026-05-18 05:51:16 +0800

    Add files via upload

---
+++ NonmetalGas.java
@@ -0,0 +1,6 @@
+//childclass
+class NonmetalGas extends Gas {
+    public NonmetalGas(String name, String symbol, int atomicNumber, double atomicMass, String electronConfig, String realWorldUse) {
+        super(name, symbol, atomicNumber, atomicMass, electronConfig, realWorldUse);
+    }
+}
