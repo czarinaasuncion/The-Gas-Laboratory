efd897a6a71b9b6624c8f5d37350b5bc0444385e
Czarina Asuncion <czarinaasuncion7197@gmail.com>
2026-05-18 19:40:13 +0800

    Update Gas.java

--- Gas.java
+++ Gas.java
@@ -1,10 +1,12 @@
 
-// base class
+// base class/ parent class
 class Gas {
+    // private final para di mabago
     private final String name, symbol, electronConfig, realWorldUse;
     private final int atomicNumber;
     private final double atomicMass;
 
+    // constructor for each element, this keyword
     public Gas(String name, String symbol, int atomicNumber, double atomicMass, String electronConfig, String realWorldUse) {
         this.name = name;
         this.symbol = symbol;
